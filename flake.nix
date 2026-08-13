a9876c39cf3a630bd80ea289636fbce5d9fa867d
boatette <boatette@gmail.com>
2026-08-13 21:25:55 +1000

    build(nix): add starter flake

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  description = "a desktop selection box for Wayland compositors";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  # outputs =  {
+  # };
+}
